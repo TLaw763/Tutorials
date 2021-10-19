fc23e540a38e48db4cef994d35ea0407e9cabe55
TLaw763 <diego.aqu@outlook.com>
2021-10-19 14:22:41 +0200

    Initial Commit

---
+++ Tutorials/Stacks.swift
@@ -0,0 +1,20 @@
+//
+//  Stacks.swift
+//  Tutorials
+//
+//  Created by Diego on 2021/10/05.
+//
+
+import SwiftUI
+
+struct Stacks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Stacks_Previews: PreviewProvider {
+    static var previews: some View {
+        Stacks()
+    }
+}
---
+++ Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by Diego on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
